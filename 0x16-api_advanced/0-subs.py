1f16385ab37c5d6f304653ec4794dc882c50e9da
Hector Macassa <cbmacassa@gmail.com>
2024-06-06 18:15:38 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-"""
-Module for querying the number of subscribers on a subreddit.
-
-This module provides a function to retrieve the number of subscribers
-for a given subreddit using the Reddit API.
+"""This module provides a function to fetch the number of subscribers for a
+given subreddit on Reddit using the Reddit API.
 """
 import requests
-from requests.exceptions import RequestException
 
 
 def number_of_subscribers(subreddit):
     """
-    Return the total number of subscribers on a subreddit.
+    Retrieves the number of subscribers for a given subreddit.
 
     Args:
-        subreddit (str): The name of the subreddit to query.
+        subreddit (str): Name of the subreddit to fetch the subscriber count
 
     Returns:
-        int: The number of subscribers for the given subreddit.
-             Returns 0 if an error occurs.
+        int: The number of subscribers for the subreddit, or 0 if the subreddit
+            is invalid or doesn't exist.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
 
     try:
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
         response.raise_for_status()
-    except RequestException:
+    except requests.exceptions.HTTPError:
+        # If the subreddit doesn't exist or the request fails, return 0
         return 0
-    else:
-        data = response.json()
-        return data.get('data', {}).get('subscribers', 0)
+
+    # If the response is a redirect to a search page, the subreddit doesn't
+    # exist
+    if response.url != url:
+        return 0
+
+    data = response.json()
+    subscribers = data["data"]["subscribers"]
+
+    return subscribers
