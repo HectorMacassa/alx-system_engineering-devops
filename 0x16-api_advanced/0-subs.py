0847ce77403de46dcbb59552e22520dac23d4f02
Hector Macassa <cbmacassa@gmail.com>
2024-06-18 21:21:35 -0700

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,20 @@
 """
 Script that queries number of subscribers on subreddit.
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'mozilla/5.0'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
+
         if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
         else:
             return 0
-    except Exception as e:
+    except requests.RequestException:
         return 0
