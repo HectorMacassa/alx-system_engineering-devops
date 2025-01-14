6f541106d0feb7a73c2f6ab6f812444b7486f976
Hector Macassa <cbmacassa@gmail.com>
2025-01-14 06:59:01 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 #!/usr/bin/env puppet
 # Installs Flask from pip3
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+package { 'python3-pip':
+  ensure  => installed,
+}
+
+exec {'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
